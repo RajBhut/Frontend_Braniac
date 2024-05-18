88e4ef43207476720169330fff3db4740729c2d7
Raj_Bhut <rajbhut2832005@gmail.com>
2024-05-18 11:11:23 +0530

    C:/Program Files (x86)/Git/create modifed

--- BranicBlast/src/componant/DashBord.jsx
+++ BranicBlast/src/componant/DashBord.jsx
@@ -3,13 +3,15 @@ import './DashBord.css'
 import { Link } from 'react-router-dom'
 import { useAuth0 } from "@auth0/auth0-react";
 export default function DashBord() {
-  const { logout , isAuthenticated  , loginWithRedirect} = useAuth0();
+  const { logout , isAuthenticated  , loginWithRedirect , user} = useAuth0();
 
 
   return (
     <>
    <div className="container">
     <h1 >DashBoard</h1>
+    <center>{isAuthenticated ? <h2>Welcome {user.name}</h2> : <h2>Welcome Guest</h2>}
+    </center>
     <div className="btn">
 
 <button className='bt'>
