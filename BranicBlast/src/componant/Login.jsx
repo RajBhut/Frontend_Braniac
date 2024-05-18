01947060fcf7e11197cd354e50af2940888c4a71
Raj_Bhut <rajbhut2832005@gmail.com>
2024-05-18 10:04:40 +0530

    clearing login page

--- BranicBlast/src/componant/Login.jsx
+++ BranicBlast/src/componant/Login.jsx
@@ -5,114 +5,114 @@ import { Link } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 const Login = () => {
   const { loginWithRedirect , isAuthenticated , user } = useAuth0();
-  const [confirmPasswordMatch, setConfirmPasswordMatch] = useState(true); // Add this line
-  const [confirmPassword, setConfirmPassword] = useState(''); // Add this line
-  const [mode, setMode] = useState('login'); // Initial mode: login
-  const [isAthenticate , SetAuthantication] = useState(false)
+//   const [confirmPasswordMatch, setConfirmPasswordMatch] = useState(true); // Add this line
+//   const [confirmPassword, setConfirmPassword] = useState(''); // Add this line
+//   const [mode, setMode] = useState('login'); // Initial mode: login
+//   const [isAthenticate , SetAuthantication] = useState(false)
   
-  const [formData, setFormData] = useState({
-    email: '',
-    password: '',
-    username: ''
-  });
-  const [errors, setErrors] = useState({}); // To store validation errors
+//   const [formData, setFormData] = useState({
+//     email: '',
+//     password: '',
+//     username: ''
+//   });
+//   const [errors, setErrors] = useState({}); // To store validation errors
 
-  const handleChange = (event) => {
-    setFormData({ ...formData, [event.target.name]: event.target.value });
-  };
+//   const handleChange = (event) => {
+//     setFormData({ ...formData, [event.target.name]: event.target.value });
+//   };
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    setErrors({}); // Clear previous errors
-    console.log(formData.password , formData.email )
-    if(confirmPassword != formData.password) {
-    console.log("Wrong match")
-      setConfirmPasswordMatch(false);
-      return;
+//   const handleSubmit = async (event) => {
+//     event.preventDefault();
+//     setErrors({}); // Clear previous errors
+//     console.log(formData.password , formData.email )
+//     if(confirmPassword != formData.password) {
+//     console.log("Wrong match")
+//       setConfirmPasswordMatch(false);
+//       return;
 
-    }
+//     }
 
-    try {
-      const { email, password  , username} = formData;
+//     try {
+//       const { email, password  , username} = formData;
 
-      const url = mode === 'login' ? 'https://brainac-blast-backend.vercel.app/login' : 'https://brainac-blast-backend.vercel.app/signup';
-      const response = await axios.post(url, formData);
+//       const url = mode === 'login' ? 'https://brainac-blast-backend.vercel.app/login' : 'https://brainac-blast-backend.vercel.app/signup';
+//       const response = await axios.post(url, formData);
 
-      console.log('Success:', response.data); 
-      SetAuthantication(true);
+//       console.log('Success:', response.data); 
+//       SetAuthantication(true);
 
-    } catch (error) {
-      if (error.response) {
-        setErrors(error.response.data); 
+//     } catch (error) {
+//       if (error.response) {
+//         setErrors(error.response.data); 
 
-      } else {
-        console.error('Error:', error.message); // Log other errors
-      }
-    }
-  };
+//       } else {
+//         console.error('Error:', error.message); // Log other errors
+//       }
+//     }
+//   };
 
-  const handleModeChange = () => {
-    setMode(mode === 'login' ? 'signup' : 'login');
-  };
+//   const handleModeChange = () => {
+//     setMode(mode === 'login' ? 'signup' : 'login');
+//   };
 
-  return (
-    <div className="auth-form">
-      <h1>{mode === 'login' ? 'Login' : 'Sign Up'}</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-        <input
-          placeholder='Username'
-            type="text"
-            id="username"
-            name="username"
-            value={formData.username}
-            onChange={handleChange}
-            required
-          />
-          <input
-          placeholder='Email'
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-          {errors.email && <p className="error">{errors.email}</p>}
-        </div>
-        <div className="form-group">
+//   return (
+//     <div className="auth-form">
+//       <h1>{mode === 'login' ? 'Login' : 'Sign Up'}</h1>
+//       <form onSubmit={handleSubmit}>
+//         <div className="form-group">
+//         <input
+//           placeholder='Username'
+//             type="text"
+//             id="username"
+//             name="username"
+//             value={formData.username}
+//             onChange={handleChange}
+//             required
+//           />
+//           <input
+//           placeholder='Email'
+//             type="email"
+//             id="email"
+//             name="email"
+//             value={formData.email}
+//             onChange={handleChange}
+//             required
+//           />
+//           {errors.email && <p className="error">{errors.email}</p>}
+//         </div>
+//         <div className="form-group">
           
-          <input
-          placeholder='Password'
-            type="password"
-            id="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
-          {errors.password && <p className="error">{errors.password}</p>}
-        </div>
-        {mode === 'signup' && (
-          <div className="form-group">
+//           <input
+//           placeholder='Password'
+//             type="password"
+//             id="password"
+//             name="password"
+//             value={formData.password}
+//             onChange={handleChange}
+//             required
+//           />
+//           {errors.password && <p className="error">{errors.password}</p>}
+//         </div>
+//         {mode === 'signup' && (
+//           <div className="form-group">
             
-            <input placeholder='Conform Password' type="text" value={confirmPassword} onChange={(e)=>setConfirmPassword(e.target.value)} className="conform" />
-          </div>
-        )}
-        <button type="submit">{mode === 'login' ? 'Login' : 'Sign Up'}</button>
-      </form>
-      <p>
-        {mode === 'login' ? 'Don\'t have an account?' : 'Already have an account?'}
-        <span onClick={handleModeChange}> {mode === 'login' ? 'Sign Up' : 'Login'}</span>
-      </p>
-      {errors.message && <p className="error">{errors.message}</p>}
-{(isAthenticate || isAuthenticated) &&
-<Link to={'/deshbord'}>enter</Link>
-}
+//             <input placeholder='Conform Password' type="text" value={confirmPassword} onChange={(e)=>setConfirmPassword(e.target.value)} className="conform" />
+//           </div>
+//         )}
+//         <button type="submit">{mode === 'login' ? 'Login' : 'Sign Up'}</button>
+//       </form>
+//       <p>
+//         {mode === 'login' ? 'Don\'t have an account?' : 'Already have an account?'}
+//         <span onClick={handleModeChange}> {mode === 'login' ? 'Sign Up' : 'Login'}</span>
+//       </p>
+//       {errors.message && <p className="error">{errors.message}</p>}
+ {(isAuthenticated) &&
+ <Link to={'/deshbord'}> <button>enter</button></Link>
+ }
 
 <button onClick={() => loginWithRedirect()}>Log In</button>
-    </div>
-  );
+//</div>
+//  );
 };
 
 export default Login;
