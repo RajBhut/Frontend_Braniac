48cc1c15fac31609047c250e4c82219229c9fc30
Raj_Bhut <rajbhut2832005@gmail.com>
2024-05-19 12:27:16 +0530

    add ui changes

--- BranicBlast/src/componant/Home.jsx
+++ BranicBlast/src/componant/Home.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './home.css'; // Import your CSS file
 
-export default function Home()
-{
- return (
-   <div>
-     <h1>Home Page</h1>
-     <Link to="/login">Login</Link>
-   </div>
- );
+export default function Home() {
+    return (
+        <div className="home-container">
+            <header className="home-header">
+                <h1>Welcome to Our Quiz App!</h1>
+                <p>Test your knowledge and improve your skills. Start quizzing today!</p>
+            </header>
+            <div className="home-content">
+                <h2>Ready to Challenge Yourself?</h2>
+                <p>Click the button below to start a quiz.</p>
+            </div>
+            <div className="home-actions">
+                <Link to="/login" className="home-link">Start Quiz</Link>
+            </div>
+        </div>
+    );
 }
